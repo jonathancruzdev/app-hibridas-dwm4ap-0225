c07683b4ea8f3cee7fc70cc5330988934410866a
Jonathan Cruz <jonathan.cruz@davinci.edu.ar>
2025-08-26 10:40:05 -0300

    Semana 03 - Servidor con Express

--- semana_03/server.js
+++ semana_03/server.js
@@ -1,5 +1,43 @@
+const chalk = require('chalk');
+const express = require('express');
+const Product = require('./Product');
+const port = 3000;
+const app = express();
 
-import chalk from "chalk";
-console.log( chalk.bgGreen('API REST'));
+const modelProduct = new Product();
+
+const home = (request, response) =>{
+    console.log( chalk.red('Ruta  Home'));
+    response.send(`
+            <h1>Hola desde Express! 😎</h1>
+            <ul>
+                <li><a href="/contactos">Contactos</a></li>
+                <li><a href="/productos">Productos</a></li>
+            </ul>
+        `);
+}
+
+app.get('/', home );
+
+app.get('/contactos', ( request, response) => {
+    response.send('<h1>Contactos</h1>');
+})
+
+app.get('/api/productos', async ( request, response) => {
+    const list = await modelProduct.getProducts();
+    console.table(list);
+    response.json(list);
+})
+
+app.get('/api/productos/:id', async (request, response) => {
+    const id = request.params.id;
+    console.log(id);
+    response.json({ id});
+})
+
 
 console.log(  chalk.green(' Hola!')  );
+
+app.listen( port, () => {
+    console.log(`Servidor en el puerto ${port}`);
+})
