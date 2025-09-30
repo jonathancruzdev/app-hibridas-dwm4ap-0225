453d8a69ec3cc5d6345f290b4b444f73aa63d8d9
Jonathan Cruz <jonathan.cruz@davinci.edu.ar>
2025-09-30 10:40:28 -0300

    Semana 08 - React: Renderdizados y props

--- semana_08/src/App.jsx
+++ semana_08/src/App.jsx
@@ -1,27 +1,67 @@
-
 import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
 
+function Curso(props) {
+  const { titulo, precio } = props;
+  return (
+    <div className='card'>
+      <h4>{ titulo }</h4>
+      <p>Descripción del Curso</p>
+      <strong>$ {precio}</strong>
+      <button>Comprar</button>
+    </div>
+  )
+}
+
+
 function App() {
-
-  const nombre = "Jonathan";
-  const fecha = "29-09-25";
-  const titulo = <h2> Soy otro título { fecha }</h2>;
-
-  const convertir = (texto) => {
-    return texto.toUpperCase();
-  }
-
-  const doble = (numero) => {
-    return numero *2;
-  }
-
+  const url = 'https://cdn.prod.website-files.com/662fb92f905585b61b12afd8/666af67c534d424825188e46_loop_pcy%20saying%20hi%20crop.gif';
+  let descripcion = "GIf de Pocoyo";
+  let logueado = true;
+  const texto = logueado ? 'Bienvenido' : 'No autorizado';
+  const cursos = [
+    {id: 1, titulo: 'Aprenda HTML', precio: 20000 },
+    {id: 2, titulo: 'Fundamentos de JavaScript', precio: 25000 },
+    {id: 3, titulo: 'Diseño Responsive', precio: 30000 },
+    {id: 4, titulo: 'Backend con PHP', precio: 21000 }
+  ];
+  //const result = cursos.map( item => item.titulo.toUpperCase()  );
+  // console.log(result)
   return (
     <>
-      { /* comentario   multi línea  */ }
-      <h1> REACT { convertir(nombre) } { titulo } </h1>
-      <br/>
-      <p className='rojo'>texto</p>
+      <h1>Cursos</h1>
+      <h2> { texto }</h2>
+      {
+        cursos.map( item => <Curso titulo={item.titulo} precio={item.precio} />  )
+      }
+      
+     
+
+      <p>Web de cursos </p>
+      <hr />
+      <ul>
+        {
+           cursos.map(  item =>  <li key={item.id}> 
+                                    <strong>{ item.titulo }</strong> |  
+                                    <span> ${ item.precio } </span>
+                                  </li> )
+        }
+      </ul>
+
+      {
+        logueado ? (
+          <img 
+            className='animacion' 
+            src={url} 
+            alt="Pocoyo" 
+            title={descripcion} 
+          />
+        ) : (
+          <h2> Chau!</h2>
+        )
+      }
+      
     </>
   )
 }
