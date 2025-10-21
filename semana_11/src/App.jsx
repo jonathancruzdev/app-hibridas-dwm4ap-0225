b3ad96a0b385926c00c6b6c057ff69eeb99b00a9
Jonathan Cruz <jonathan.cruz@davinci.edu.ar>
2025-10-21 10:47:19 -0300

    Semana 11 - useEffect y fetch

--- semana_11/src/App.jsx
+++ semana_11/src/App.jsx
@@ -1,83 +1,32 @@
-
-import { useState, useEffect } from 'react'
+import { useState, useEffect, use } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
 import Header from './components/Header'
 import Nav from './components/Nav' 
-import Tareas from './components/Tareas'
-import Tarea from './components/Tarea'
 import Footer from './components/Footer'
-// Repaso de destructuración
-const persona = {nombre: 'Juan', edad: 27, fn: function(){ console.log('Hola')}};
-const {nombre, edad, fn } = persona;
-console.log(nombre, edad, fn);
-//                     0             1              2          3
-const materias = ['Maquetado', 'Programación I', 'PWA', ()=>console.log('hola') ];
-const [m1, m2, m3, fn2] =  materias;
-console.log(m1, m2)
-fn2();
+import Loading from './components/Loading'
 
-
+import Home from './views/Home'
+import Login from './views/Login'
+import Register from './views/Register'
 
 function App() {
   const usuario = 'Jonathan';
-  const api = 'https://apitaskdwm4ap-5ml6.onrender.com/api'
-  const endPoint = `${api}/tasks`;
-
-  // setInterval( () => {  }, 1000);
-
-  useEffect( () =>  {
-    getTasks();
-  }, [] );
-
-  const getTasks = async () => {
-    const resp = await fetch(endPoint);
-    const data = await resp.json();
-    console.log( data );
-    setTareas( data.data  );
-  }
-
-  const [ descripcion, setDescripcion] = useState('');
-  const [ tareas, setTareas] = useState([]);
-
-  const manejadorSubmit = (event) => {
-    event.preventDefault();
-    //const descripcion = 'Tarea de ejemplo';
-    const fecha = new Date().toLocaleDateString();
-    const id = tareas.length + 1;
-    const tarea = { id, descripcion, fecha};
-    console.log(tarea);
-    //             estado  , nuevo    
-    setTareas( [ ...tareas, tarea ] );
-    setDescripcion('');
-  }
+ 
   return (
     <>
       <Header>
         <Nav usuario={usuario}/>
       </Header>
-      <main className='container'>
 
-    
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={ <Home />} />
+          <Route path='/login' element={ <Login />} />
+          <Route path='/register' element={ <Register />} />
+        </Routes>
+      </BrowserRouter>
 
-        <form onSubmit={manejadorSubmit} className="nueva-tarea">
-          <input
-            onChange={ e => setDescripcion(e.target.value)  }
-            value={descripcion} 
-            type="text" 
-            name="nueva"
-            placeholder='Nueva Tarea'
-            required
-            />
-          <button  type='submit' className=''><i className="fa-solid fa-pencil"></i> Crear</button>
-        </form>
-        <Tareas>
-          {
-            tareas.map( ( item) =>  <Tarea key={item._id} descripcion={item.descripcion} fecha={item.fecha} />)
-          }
-        </Tareas>
-        <h2>Tareas Pendientes <span> { tareas.length }</span></h2>
-
-      </main>
       <Footer descripcion="DV | ToDo APP" />
     </>
   )
---
+++ semana_11/src/views/Home.jsx
@@ -0,0 +1,83 @@
+import Tareas from "../components/Tareas"
+import Tarea from "../components/Tarea"
+import Loading from "../components/Loading"
+
+import { useState, useEffect, use } from 'react'
+
+
+const Home = () => {
+
+    const api = 'http://127.0.0.1:5000/api/tasks';
+    const endPoint = `${api}/tasks`;
+
+  // setInterval( () => {  }, 1000);
+  useEffect( () =>  {
+    getTasks();
+  }, [] );
+
+  const getTasks = async () => {
+    const resp = await fetch(endPoint);
+    const data = await resp.json();
+    console.log( data );
+    setTareas( data.data  );
+    setLoading(false);
+  }
+
+  const [ descripcion, setDescripcion] = useState('');
+  const [ tareas, setTareas] = useState([]);
+  const [ loading, setLoading ] = useState( true );
+
+  const manejadorSubmit = async (event) => {
+    event.preventDefault();
+    setLoading(true);
+    const endPoint = `${api}/tasks`;
+    const option = {
+      method: 'POST',
+      headers:{
+        'Content-Type': 'application/json'
+      }, 
+      body: JSON.stringify( {descripcion} )
+    }
+    const response = await fetch( endPoint, option );
+    const json = await response.json();
+
+    const fecha = json.data.fecha;
+    const _id = json.data._id;
+    const tarea = { _id, descripcion, fecha};
+    console.log(tarea);
+    //             estado  , nuevo    
+    setTareas( [ ...tareas, tarea ] );
+    setDescripcion('');
+    setLoading(false);
+  }
+
+
+    return (
+        <main className='container'>
+
+        { loading ? <Loading /> : <></> }
+        
+
+        <form onSubmit={manejadorSubmit} className="nueva-tarea">
+          <input
+            onChange={ e => setDescripcion(e.target.value)  }
+            value={descripcion} 
+            type="text" 
+            name="nueva"
+            placeholder='Nueva Tarea'
+            required
+            />
+          <button  type='submit' className=''><i className="fa-solid fa-pencil"></i> Crear</button>
+        </form>
+        <Tareas>
+          {
+            tareas.map( ( item) =>  <Tarea key={item._id} descripcion={item.descripcion} fecha={item.fecha} />)
+          }
+        </Tareas>
+        <h2>Tareas Pendientes <span> { tareas.length }</span></h2>
+
+      </main>
+    )
+}
+
+export default Home
